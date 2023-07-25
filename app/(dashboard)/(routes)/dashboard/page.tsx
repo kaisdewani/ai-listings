7aca4d920e14c1dbee6a7dc053c71fd3958091fe
Kais <kaisdewani@gmail.com>
2023-07-25 16:50:30 -0500

    add sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
-    <p>Dashboard Page (Protected)</p>
-  )
-}
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
 
 export default DashboardPage;
